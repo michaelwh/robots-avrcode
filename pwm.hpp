a9082751d6673e5e74bb045deedb2e4bd4ef3ca2
unknown <rt5g11@F10.ecs.soton.ac.uk>
2012-05-16 12:16:37 +0100

    PWM movement

--- pwm.hpp
+++ pwm.hpp
@@ -9,10 +9,14 @@
 #define PWM_HPP_
 
 class PWM {
+
 public:
-	static void init();
-	static uint8_t TopServoMove(uint16_t pwm_value);
-	static uint8_t BottomServoMove(uint16_t pwm_value);
+	 PWM(void);
+	 uint16_t _current_value_top;
+	 uint16_t _current_value_bottom;
+
+	 uint8_t TopServoMove(uint16_t pwm_value);
+	 uint8_t BottomServoMove(uint16_t pwm_value);
 };
 
 
